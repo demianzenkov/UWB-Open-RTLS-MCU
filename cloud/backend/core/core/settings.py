1b550616014297a0c91b920406d0ea58ad2fb589
Igor Nikolaev <oldsantiago21@gmail.com>
2019-03-29 22:10:30 +0300

    feat: django shit

--- cloud/backend/core/core/settings.py
+++ cloud/backend/core/core/settings.py
@@ -74,12 +74,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'core',
-    #     'HOST': 'host_name',
-    #     'PORT': '11111'
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'core',
+        'HOST': 'db',
+        'PORT': '5432',
+        'USER': 'root',
+        'PASSWORD': 'root'
+    }
 }
 
 
