f9c4d16d6e8d10d8f567685333e677cd34039c3f
demianzenkov <demianzenkov@gmail.com>
2019-03-30 19:23:09 +0300

    feat: zmq server skeleton

--- orange_pi/serial_port/serialport.py
+++ orange_pi/serial_port/serialport.py
@@ -1,14 +1,52 @@
+import time
 import asyncio
 import serial_asyncio
+import zmq
+from zmq.asyncio import Context, Poller
 
 
-async def main(loop):
-    reader, writer = await serial_asyncio.open_serial_connection(url='/dev/tty.usbserial-A9A51FF3', baudrate=115200)
+url = 'tcp://127.0.0.1:5555'
+ctx = Context.instance()
+# queue = asyncio.Queue()
+
+
+async def receiver():
+    """receive messages with polling"""
+    pull = ctx.socket(zmq.PULL)
+    pull.connect(url)
+    poller = Poller()
+    poller.register(pull, zmq.POLLIN)
+    while True:
+        events = await poller.poll()
+        if pull in dict(events):
+            # print("recving", events)
+            msg = await pull.recv_multipart()
+            print('recvd', msg)
+
+
+async def sender():
+    """send a message every second"""
+    tic = time.time()
+    push = ctx.socket(zmq.PUSH)
+    push.bind(url)
+    while True:
+        print("sending")
+        await push.send_multipart([str(time.time() - tic).encode('ascii')])
+        await asyncio.sleep(1)
+
+
+
+async def main():
+    # reader, writer = await serial_asyncio.open_serial_connection(url='/dev/tty.usbserial-A9A51FF3', baudrate=115200)
+    reader, writer = await serial_asyncio.open_serial_connection(url='reader', baudrate=115200)
     print('Reader, writer created')
     messages = [b'foo\n', b'bar\n', b'baz\n', b'qux\n']
     # sent = send(writer, messages)
     received = receive(reader)
-    await asyncio.wait({received})
+    await asyncio.wait([received,
+                        # ping(),
+                        receiver(),
+                        sender()])
 
 
 async def send(w, msgs):
@@ -29,5 +67,6 @@ async def receive(r):
 
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(main(loop))
+loop.run_until_complete(main())
 loop.close()
+
--- orange_pi/socket_server/app.py
+++ orange_pi/socket_server/app.py
@@ -6,16 +6,20 @@ import zmq.asyncio
 
 message_queue = asyncio.Queue()
 
+
 async def pzm():
     # todo: zmq listener
     while True:
         await message_queue.put('>>>>>>')
         await asyncio.sleep(2.0)
 
+
 async def websocket_handler(request):
 
     ws = web.WebSocketResponse()
     await ws.prepare(request)
+    async for msg in ws:
+        print(msg.data)
 
     while True:
         message = await message_queue.get()
@@ -24,8 +28,10 @@ async def websocket_handler(request):
         else:
             await asyncio.sleep(0.5)
 
+
 app = web.Application()
 app.add_routes([web.get('/ws', websocket_handler)])
+
 
 async def start_background_tasks(app):
     app['zmq_listener'] = app.loop.create_task(pzm())
