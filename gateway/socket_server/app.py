05690ea9f6645fad2d133876ccafa3b86a057c7b
Igor Nikolaev <oldsantiago21@gmail.com>
2019-04-03 03:00:08 +0300

    wip

--- gateway/socket_server/app.py
+++ gateway/socket_server/app.py
@@ -2,7 +2,18 @@ import aiohttp
 from aiohttp import web
 import asyncio
 import zmq
+import logging
+import sys
 from zmq.asyncio import Context
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s : [%(levelname)s] : %(message)s')
+handler = logging.StreamHandler(sys.stdout)
+
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 
 _WS_SLEEP_TIME = 0.5
@@ -10,34 +21,42 @@ _ZMQ_PAIR_SLEEP = 0.5
 
 _ZMQ_URL = 'tcp://localhost:9999'
 
-message_queue = asyncio.Queue()
-
 context = Context.instance()
+socket = context.socket(zmq.PAIR)
+socket.connect(_ZMQ_URL)
 
+async def zmq_send(message: str):
+    await socket.send_string(message)
 
 async def zmq_receiver():
-    socket = context.socket(zmq.PAIR)
-    socket.connect(_ZMQ_URL)
     while True:
+        client_ws = app['clients'].get('cloud_ws')
         message = await socket.recv_string()
-        await message_queue.put(message)
+        logger.info(f'Received from ZMQ Pair: {message}')
+        if client_ws is not None:
+            await client_ws.send_str(message)
+            logger.info(f'Message {message} sent via websocket to cloud')
         await asyncio.sleep(_ZMQ_PAIR_SLEEP)
 
 
 async def websocket_handler(request):
+    secret = request.query.get('secret')
+    if not secret:
+        return web.Response(status=403)
 
     ws = web.WebSocketResponse()
     await ws.prepare(request)
-
-    while True:
-        message = await message_queue.get()
-        if message:
-            await ws.send_str(message)
-        else:
-            await asyncio.sleep(_WS_SLEEP_TIME)
+    app['clients']['cloud_ws'] = ws
+    async for message in ws:
+        logger.info(f'Socket received message from cloud: {message.data}')
+        # await zmq_send(message.data)
+        logger.info(f'Socket sent message to zmq: {message.data}')
+    await ws.close()
+    return ws
 
 
 app = web.Application()
+app['clients'] = {}
 app.add_routes([web.get('/', websocket_handler)])
 
 
