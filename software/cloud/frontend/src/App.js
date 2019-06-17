65bb04ea23f3886e80525ad7bb2969792b2648fd
Igor Nikolaev <oldsantiago21@gmail.com>
2019-06-17 18:51:32 +0300

    feat: some tweaks

--- software/cloud/frontend/src/App.js
+++ software/cloud/frontend/src/App.js
@@ -1,12 +1,11 @@
-import React, { Component } from 'react';
-import Monitor from './Monitor';
+import React, { Component } from "react";
+import Monitor from "./Monitor";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        App
-        <Monitor></Monitor>
+        <Monitor />
       </div>
     );
   }
--- software/cloud/frontend/src/Monitor.js
+++ software/cloud/frontend/src/Monitor.js
@@ -1,27 +1,86 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Monitor extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
-    this.state = {'x': 0}
+    this.state = {
+      dots: [
+        { k: 1, x: 250, y: 90, vX: -1, vY: -1 },
+        { k: 2, x: 150, y: 45, vX: -1, vY: 1 },
+        { k: 3, x: 900, y: 70, vX: 1, vY: -1 },
+        { k: 4, x: 700, y: 10, vX: 1, vY: 1 },
+        { k: 5, x: 900, y: 120, vX: 1, vY: -1 },
+        { k: 6, x: 200, y: 50, vX: 1, vY: 1 }
+      ]
+    };
 
-    setInterval(()=>{
-      this.setState({'x': this.state.x + 1})
-      if (this.state.x > 100) {
-        this.setState({'x': 0})
-      }
-    }, 10)
+    setInterval(() => {
+      this.state.dots.forEach(d => {
+        if (d.x + d.vX === 1000) {
+          const newDot = { k: d.k, x: 999, y: d.y, vY: d.vY, vX: -1 };
+          this.setState({
+            dots: [...this.state.dots.filter(dd => dd.k !== d.k), newDot]
+          });
+          return;
+        }
+
+        if (d.x + d.vX === 0) {
+          const newDot = { k: d.k, x: 1, y: d.y, vY: d.vY, vX: 1 };
+          this.setState({
+            dots: [...this.state.dots.filter(dd => dd.k !== d.k), newDot]
+          });
+          return;
+        }
+
+        if (d.y + d.vY === 500) {
+          const newDot = { k: d.k, x: d.x, y: d.y, vY: -1, vX: d.vX };
+          this.setState({
+            dots: [...this.state.dots.filter(dd => dd.k !== d.k), newDot]
+          });
+          return;
+        }
+
+        if (d.y + d.vY === 0) {
+          const newDot = { k: d.k, x: d.x, y: d.y, vY: 1, vX: d.vX };
+          this.setState({
+            dots: [...this.state.dots.filter(dd => dd.k !== d.k), newDot]
+          });
+          return;
+        }
+
+        const newDot = {
+          k: d.k,
+          x: d.x + d.vX,
+          y: d.y + d.vY,
+          vY: d.vY,
+          vX: d.vX
+        };
+        this.setState({
+          dots: [...this.state.dots.filter(dd => dd.k !== d.k), newDot]
+        });
+      });
+    }, 10);
   }
 
-  componentDidMount() {
-  }
+  componentDidMount() {}
 
   render() {
     return (
       <div>
-        <svg width="500" height="500" style={{'backgroundColor': 'yellow'}}>
-          <circle cx={this.state.x} cy="25" r='5'></circle>
+        <svg
+          width="1000"
+          height="500"
+          style={{
+            backgroundColor: "yellow",
+            marginTop: "6rem",
+            marginLeft: "2rem",
+            border: "1px solid black"
+          }}
+        >
+          {this.state.dots.map(d => (
+            <circle key={d.k} cx={d.x} cy={d.y} r="5px" />
+          ))}
         </svg>
       </div>
     );
