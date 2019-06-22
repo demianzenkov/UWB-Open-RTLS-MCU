d426244a5371ec666da59bd9e4a959ff59a17d62
Igor Nikolaev <oldsantiago21@gmail.com>
2019-06-22 22:06:17 +0300

    feat: some django model shit

--- software/cloud/backend/core/devices/admin.py
+++ software/cloud/backend/core/devices/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+class AnchorAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Anchor, AnchorAdmin)
