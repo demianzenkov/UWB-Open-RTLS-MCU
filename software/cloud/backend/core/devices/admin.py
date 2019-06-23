39b182608165c1d277dcf2d271ee65aba5f67f70
Igor Nikolaev <oldsantiago21@gmail.com>
2019-06-23 23:34:54 +0300

    feat: send read and write requests to station

--- software/cloud/backend/core/devices/admin.py
+++ software/cloud/backend/core/devices/admin.py
@@ -1,10 +1,28 @@
+import logging
 from django.contrib import admin
 from .models import *
+from services.udp_sender.senders import send_read_network_settings_command, send_write_network_settings_command
 
+logger = logging.getLogger('root')
+
+def read_network_settings(modeladmin, request, queryset):
+    for tag in queryset:
+        try:
+            send_read_network_settings_command(tag.ip)
+        except Exception as e:
+            logger.exception(e)
+
+def write_network_settings(modeladmin, request, queryset):
+    for tag in queryset:
+        try:
+            send_write_network_settings_command(tag.ip)
+        except Exception as e:
+            logger.exception(e)
 
 class TagAdmin(admin.ModelAdmin):
-    pass
-
+    
+    actions = [read_network_settings, write_network_settings]
+    
 
 class AnchorAdmin(admin.ModelAdmin):
     pass
